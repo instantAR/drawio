1efc2feaef1a746744bc795bbdad494865edf0f8
jituhooda <42730609+jituhooda@users.noreply.github.com>
2024-10-30 22:04:59 -0700

    adding changes for AppBuilder url

--- src/main/webapp/js/enviroment.js
+++ src/main/webapp/js/enviroment.js
@@ -1,7 +1,7 @@
 var enviroment = {
     'restClientService': window?.configs?.['restClientService'] || 'https://connect.instantar.io/restapi/',
     'restBackendService': window?.configs?.['restBackendService'] || 'https://connect.instantar.io/restapi/',
-    'appBuilder': window?.configs?.['appBuilder'] || 'https://dev24.api.instantar.io/api/v2.0/'
+    'appBuilder': window?.configs?.['appBuilder'] || 'https://dev.emachine.ai/api/api/v2.0/'
 }
 
 window.enviroment = enviroment;
